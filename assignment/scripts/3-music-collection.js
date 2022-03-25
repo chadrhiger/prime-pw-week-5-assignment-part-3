3ff7a347ce020afeff458e9b6c6d1d5e0544a5d1
Chad Rhiger <csrhig@gmail.com>
2022-03-25 15:46:54 -0500

    created empty ARRAY named collection. created FUNCTION that took in 3 parameters named addToCollection. added 6 albums that included artist and yearPublished. logged function and it came back with all relaevant info.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,26 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+  // code to be executed
+  let album = {};
+  album.title = title;
+  album.artist = artist;
+  album.yearPublished = yearPublished;
+    collection.push(album);
+  return album;
+}
+
+addToCollection('Siamese Dream', 'Smashing Pumpkins', 1993);
+addToCollection('Tromple le Monde', 'Pixies', 1991);
+addToCollection('Gish', 'Smahing Pumkins', 1991);
+addToCollection('Last Splash', 'The Breeders', 1993);
+addToCollection('Come On Die Young', 'Mogwai', 1999);
+addToCollection('()', 'Sigur Ros', 2002);
+
+console.log('collection is:', collection);
+
+function showCollection(artist){
+  console.log(artist.length);
+}
