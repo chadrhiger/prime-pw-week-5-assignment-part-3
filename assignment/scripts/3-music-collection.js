4ce4ff5ab9d89e3d21c0abc9bf5397655e6515c5
Chad Rhiger <csrhig@gmail.com>
2022-03-25 17:12:15 -0500

    added second function, showCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -8,19 +8,25 @@ function addToCollection(title, artist, yearPublished){
   album.title = title;
   album.artist = artist;
   album.yearPublished = yearPublished;
-    collection.push(album);
+  collection.push(album);
   return album;
 }
 
+addToCollection('()', 'Sigur Ros', 2002);
 addToCollection('Siamese Dream', 'Smashing Pumpkins', 1993);
 addToCollection('Tromple le Monde', 'Pixies', 1991);
-addToCollection('Gish', 'Smahing Pumkins', 1991);
+addToCollection('Gish', 'Smashing Pumkins', 1991);
 addToCollection('Last Splash', 'The Breeders', 1993);
 addToCollection('Come On Die Young', 'Mogwai', 1999);
-addToCollection('()', 'Sigur Ros', 2002);
 
 console.log('collection is:', collection);
 
-function showCollection(artist){
-  console.log(artist.length);
+function showCollection(array){
+  console.log('Number of albums in my collection:', array.length);
+  for(let index of array){
+    console.log(`${index.title} by ${index.artist}, published in ${index.yearPublished}`); 
+  }  
 }
+showCollection(collection)
+console.log(collection[0]);
+console.log(collection[0].artist);
